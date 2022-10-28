67c17d8c65f83b4aeac77d4d1adc94347cc1c7ce
Jonathan Lee <jlee1546.jl@gmail.com>
2022-10-27 22:56:03 -0400

    Create new form

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,125 +5,63 @@ function Input() {
     firstName: "",
     lastName: "",
     email: "",
-    comments: "",
-    isFriendly: true,
-    employment: "",
-    favColor: "",
+    password: "",
+    passwordConfirmation: "",
   });
 
-  function changeHandler(event) {
-    console.log(event.target.checked);
-    const { name, value, type, checked } = event.target;
-    setFormData((prevState) => {
-      return {
-        ...prevState,
-        [name]: type === "checkbox" ? checked : value,
-      };
-    });
+  function clickHandler(event) {
+    console.log(event.target.value);
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   }
 
-  function processForm(event) {
+  function submitFormData(event) {
     event.preventDefault();
     console.log(formData);
   }
 
   return (
-    <form onSubmit={processForm}>
+    <form onSubmit={submitFormData}>
       <input
+        name="firstName"
         type="text"
         placeholder="First Name"
-        name="firstName"
-        onChange={changeHandler}
         value={formData.firstName}
+        onChange={clickHandler}
       />
       <input
+        name="lastName"
         type="text"
         placeholder="Last Name"
-        name="lastName"
-        onChange={changeHandler}
         value={formData.lastName}
+        onChange={clickHandler}
       />
       <input
+        name="email"
         type="email"
         placeholder="Email"
-        name="email"
-        onChange={changeHandler}
         value={formData.email}
+        onChange={clickHandler}
       />
-      <textarea
-        placeholder="Comments"
-        name="comments"
-        onChange={changeHandler}
-        value={formData.comments}
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={clickHandler}
       />
-      <div>
-        <input
-          type="checkbox"
-          id="isFriendly"
-          onChange={changeHandler}
-          checked={formData.isFriendly}
-          name="isFriendly"
-        />
-        <label htmlFor="isFriendly">Are you friendly</label>
-      </div>
-      <fieldset>
-        <legend>Current employment status</legend>
-
-        <input
-          type="radio"
-          id="unemployed"
-          name="employment"
-          value="unemployed"
-          checked={formData.employment === "unemployed"}
-          onChange={changeHandler}
-        />
-        <label htmlFor="unemployed">Unemployed</label>
-
-        <br />
-
-        <input
-          type="radio"
-          id="full-time"
-          name="employment"
-          value="full-time"
-          checked={formData.employment === "full-time"}
-          onChange={changeHandler}
-        />
-        <label htmlFor="full-time">Full-time</label>
-        <br />
-
-        <input
-          type="radio"
-          id="part-time"
-          name="employment"
-          value="part-time"
-          checked={formData.employment === "part-time"}
-          onChange={changeHandler}
-        />
-        <label htmlFor="part-time">Part-time</label>
-      </fieldset>
-      <br />
-
-      <label htmlFor="favColor">What is your favorite color?</label>
-
-      <select
-        id="favColor"
-        value={formData.favColor}
-        onChange={changeHandler}
-        name="favColor"
-      >
-        <option value="">-- Choose --</option>
-        <option value="red">Red</option>
-        <option value="orange">Orange</option>
-        <option value="yellow">Yelloww</option>
-        <option value="green">Green</option>
-        <option value="blue">Blue</option>
-        <option value="indigo">Indigo</option>
-        <option value="violet">Violet</option>
-      </select>
+      <input
+        name="passwordConfirmation"
+        type="password"
+        placeholder="Confirm"
+        value={formData.passwordConfirmation}
+        onChange={clickHandler}
+      />
       <button>Submit</button>
     </form>
   );
 }
-
 export default Input;
